b565b7b1904173408ea684416cce458e111c92e5
Thiago Fernandes Felipe <134814091+thiagofernandesfelipe@users.noreply.github.com>
2024-07-25 18:41:02 -0300

    alteração nas pastas

--- Views/uPrincipalView.dfm
+++ Views/uPrincipalView.dfm
@@ -11,4 +11,13 @@ object Form1: TForm1
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 15
+  object Button1: TButton
+    Left = 320
+    Top = 272
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
 end
--- Views/uPrincipalView.pas
+++ Views/uPrincipalView.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uConexaoDAO;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +22,18 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  VConexao : TConexaoDAO;
+begin
+  VConexao := TConexaoDAO.Create;
+
+  VConexao.GetConn.Connected := true;
+
+  if VConexao.GetConn.Connected then
+    ShowMessage('porra do caralho buceta filha da puta!!! conectou nesse caralho');
+
+end;
 
 end.
