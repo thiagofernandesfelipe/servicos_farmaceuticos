39b593bf5834c16975c212664f8957220f7bafa4
thiagoFelipeBPM <thiago.felipe@bpm.com.br>
2024-07-26 15:08:11 -0300

    Update uProcedimentoModel.pas

--- Models/uProcedimentoModel.pas
+++ Models/uProcedimentoModel.pas
@@ -7,10 +7,13 @@ type
   private
     FValor: Double;
     FDescricao: string;
+    FId_Procedimento: integer;
     procedure SetDescricao(const Value: string);
     procedure SetValor(const Value: Double);
+    procedure SetId_Procedimento(const Value: integer);
 
   public
+    property Id_Procedimento: integer read FId_Procedimento write SetId_Procedimento;
     property Descricao: string read FDescricao write SetDescricao;
     property Valor: Double read FValor write SetValor;
 
@@ -23,6 +26,11 @@ implementation
 procedure TProcedimentoModel.SetDescricao(const Value: string);
 begin
   FDescricao := Value;
+end;
+
+procedure TProcedimentoModel.SetId_Procedimento(const Value: integer);
+begin
+  FId_Procedimento := Value;
 end;
 
 procedure TProcedimentoModel.SetValor(const Value: Double);
