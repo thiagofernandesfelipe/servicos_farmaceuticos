75ce6bfd8b6a09acf718d10f59725944bcdf1b48
Thiago Fernandes Felipe <134814091+thiagofernandesfelipe@users.noreply.github.com>
2024-07-26 20:27:16 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE farmacia_banco CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+
 CREATE TABLE servico_farmaceutico (
     id_servico INT AUTO_INCREMENT PRIMARY KEY,
     data DATE NOT NULL,
