cbcd3e64f0f0dff4867e73f96b1fb1717d291c2c
Thiago Fernandes Felipe <134814091+thiagofernandesfelipe@users.noreply.github.com>
2024-07-26 20:27:07 -0300

    Update uServicoDAO.pas

--- DAO/uServicoDAO.pas
+++ DAO/uServicoDAO.pas
@@ -13,30 +13,49 @@ type
     constructor Create;
     function GetServicos: TFDQuery;
     function GetServicoById(AIdServico: Integer): TFDQuery;
-    function Add(AServicoModel: TServicoModel): Boolean;
-    function Edit(AServicoModel: TServicoModel): Boolean;
-    function Delete(AServicoModel: TServicoModel): Boolean;
+    function Add(AServicoModel: TServicoModel): integer;
+    function Edit(AServicoModel: TServicoModel): integer;
+    function Delete(AServicoModel: TServicoModel): integer;
+    procedure SetValorTotal(AIdServico: Integer; Valor: Double);
   end;
 
 implementation
 
 { TServicoDAO }
 
-function TServicoDAO.Add(AServicoModel: TServicoModel): Boolean;
+function TServicoDAO.Add(AServicoModel: TServicoModel): integer;
+var vQuery : TFDQuery;
+    sql : string;
+begin
+  Result := 0;
+  vQuery := FConexao.CriarQuery();
+  try
+    sql := 'insert into servico_farmaceutico(data, farmaceutico, paciente, obs, valor_total)'+
+           ' values(:data, :farmaceutico, :paciente, :obs, :valor_total);'+
+           ' SELECT LAST_INSERT_ID() as id';
+
+    vQuery.Open(sql, [FormatDateTime('yyyy-mm-dd', AServicoModel.Data),
+                         AServicoModel.Farmaceutico,
+                         AServicoModel.Paciente,
+                         AServicoModel.Obs,
+                         AServicoModel.Valor_Total]);
+    Result := vQuery.FieldByName('id').AsInteger;
+  finally
+    vQuery.Free;
+  end;
+end;
+
+procedure TServicoDAO.SetValorTotal(AIdServico: Integer; Valor: Double);
 var vQuery : TFDQuery;
     sql : string;
 begin
   vQuery := FConexao.CriarQuery();
   try
-    sql := 'insert into servico_farmaceutico(data, farmaceutico, paciente, obs, valor_total)'+
-           ' values(:data, :farmaceutico, :paciente, :obs, :valor_total)';
+    sql := 'update servico_farmaceutico'+
+           ' set valor_total = :valor'+
+           ' where id_servico = :idservico';
 
-    vQuery.ExecSQL(sql, [FormatDateTime('yyyy-mm-dd', AServicoModel.Data),
-                         AServicoModel.Farmaceutico,
-                         AServicoModel.Paciente,
-                         AServicoModel.Obs,
-                         AServicoModel.Valor_Total]);
-    Result := true;
+    vQuery.ExecSQL(sql, [Valor,AIdServico]);
   finally
     vQuery.Free;
   end;
@@ -47,26 +66,28 @@ begin
   FConexao :=  TConexaoControl.GetInstance().Conexao;
 end;
 
-function TServicoDAO.Delete(AServicoModel: TServicoModel): Boolean;
+function TServicoDAO.Delete(AServicoModel: TServicoModel): integer;
 var vQuery : TFDQuery;
     sql : string;
 begin
+  Result := 0;
   vQuery := FConexao.CriarQuery();
   try
     sql := 'delete from servico_farmaceutico'+
            ' where id_servico = :idservico';
 
     vQuery.ExecSQL(sql, [AServicoModel.id_servico]);
-    Result := true;
+    Result := 1;
   finally
     vQuery.Free;
   end;
 end;
 
-function TServicoDAO.Edit(AServicoModel: TServicoModel): Boolean;
+function TServicoDAO.Edit(AServicoModel: TServicoModel): integer;
 var vQuery : TFDQuery;
     sql : string;
 begin
+  Result := 0;
   vQuery := FConexao.CriarQuery();
   try
     sql := 'update servico_farmaceutico'+
@@ -77,13 +98,13 @@ begin
            ' valor_total = :valor_total'+
            ' where id_servico = :idservico';
 
-    vQuery.ExecSQL(sql, [AServicoModel.Data,
+    vQuery.ExecSQL(sql, [FormatDateTime('yyyy-mm-dd', AServicoModel.Data),
                          AServicoModel.Farmaceutico,
                          AServicoModel.Paciente,
                          AServicoModel.Obs,
                          AServicoModel.Valor_Total,
                          AServicoModel.id_servico]);
-    Result := true;
+    Result := 1;
   finally
     vQuery.Free;
   end;
